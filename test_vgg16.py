fc4f6d178a8255e56b8a5b766438487d127683f8
yuu-eguci <yuu.eguci@gmail.com>
2021-06-13 09:12:01 +0900

    💡 Vgg16 -> VGG16

--- test_vgg16.py
+++ test_vgg16.py
@@ -1,6 +1,6 @@
 """test_vgg16
 
-keras の vgg16 モデルで画像予測(類推? 推論? 用語がわからん)のテストを行うスクリプトです。
+keras の VGG16 モデルで画像予測(類推? 推論? 用語がわからん)のテストを行うスクリプトです。
 keras-vgg16-lab repository を見たときは一番最初に実行するスクリプトです。
 
 1. 推論したい画像を自分でてきとうに用意してください。
@@ -17,20 +17,20 @@ import numpy
 
 def main(input_filename: str):
 
-    # Vgg16 をロードします。
+    # VGG16 をロードします。
     # 重みファイル(学習済みデータのこと)には imagenet を指定します。
     # NOTE: 今回は作成済みの重みファイルを使います。
     #       そのため、テストのためにわざわざ学習に時間をとることがありません。
     # NOTE: <class 'keras.engine.functional.Functional'>
     vgg16 = VGG16(weights='imagenet')
 
-    # Vgg16 の内容……サマリを見るにはこうします。
+    # VGG16 の内容……サマリを見るにはこうします。
     # 16の由来である、16層の畳み込みニューラルネットワークのサマリを閲覧できます。
     # NOTE: はい勿論ここはよくわかっていません。
     vgg16.summary()
 
     # test_vgg16.py の引数で指定した画像を読み込みます。
-    # ここでは vgg16 のデフォルトである 224x224 にリサイズしています。
+    # ここでは VGG16 のデフォルトである 224x224 にリサイズしています。
     # NOTE: 画像の入力は、 keras.preprocessing.image を使うと色々便利だそうです。
     # NOTE: <class 'PIL.Image.Image'>
     image_pil = image.load_img(input_filename, target_size=(224, 224))
@@ -48,7 +48,7 @@ def main(input_filename: str):
     image_array_4dim = numpy.expand_dims(image_array_3dim, axis=0)
 
     # 分類……与えられた画像が何なのか予測……を行います。
-    # Vgg16 に画像データを与えて、予測結果を取得します。
+    # VGG16 に画像データを与えて、予測結果を取得します。
     # NOTE: 今回利用しているデフォのモデルは、1000クラス分類のモデルです。
     #       だからここでは1000クラスの確率が結果として返ります。
     # NOTE: predictions は numpy.ndarray のリストです。
